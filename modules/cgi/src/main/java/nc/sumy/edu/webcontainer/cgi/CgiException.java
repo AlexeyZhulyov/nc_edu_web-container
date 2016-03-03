43fd885d71a0442e5ffa415737037a45d0e42c19
AlexeyZhulyov <alexey.zhulyov@gmail.com>
2016-03-04 00:35:49 +0200

    #4  Delete 2 constructors from CgiException and delete CgiExceptionTest

--- modules/cgi/src/main/java/nc/sumy/edu/webcontainer/cgi/CgiException.java
+++ modules/cgi/src/main/java/nc/sumy/edu/webcontainer/cgi/CgiException.java
@@ -2,19 +2,11 @@ package nc.sumy.edu.webcontainer.cgi;
 
 public class CgiException extends RuntimeException {
 
-    public CgiException() {
-        super();
-    }
-
     public CgiException(String message) {
         super(message);
     }
 
     public CgiException(String message, Throwable cause) {
         super(message, cause);
-    }
-
-    public CgiException(Throwable cause) {
-        super(cause);
     }
 }
