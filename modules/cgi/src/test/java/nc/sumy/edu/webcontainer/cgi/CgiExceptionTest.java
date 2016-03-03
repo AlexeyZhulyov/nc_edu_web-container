d6fc65840966fedc91df361f7ad745f332e267b5
AlexeyZhulyov <alexey.zhulyov@gmail.com>
2016-03-03 23:16:22 +0200

    #4  Fix tests

--- modules/cgi/src/test/java/nc/sumy/edu/webcontainer/cgi/CgiExceptionTest.java
+++ modules/cgi/src/test/java/nc/sumy/edu/webcontainer/cgi/CgiExceptionTest.java
@@ -1,36 +1,37 @@
 package nc.sumy.edu.webcontainer.cgi;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 
 public class CgiExceptionTest {
-    String message = "Some message";
+    final String MESSAGE = "Some message";
 
     @Rule
     public ExpectedException cgiException = ExpectedException.none();
 
-    @org.junit.Test
+    @Test
     public void testExpectedException1() {
         cgiException.expect(CgiException.class);
         throw new CgiException();
     }
 
-    @org.junit.Test
+    @Test
     public void testExpectedException2() {
         cgiException.expect(CgiException.class);
-        cgiException.expectMessage(message);
-        throw new CgiException(message);
+        cgiException.expectMessage(MESSAGE);
+        throw new CgiException(MESSAGE);
     }
 
-    @org.junit.Test
+    @Test
     public void testExpectedException3() {
         cgiException.expect(CgiException.class);
-        cgiException.expectMessage(message);
-        throw new CgiException(message, new IllegalArgumentException());
+        cgiException.expectMessage(MESSAGE);
+        throw new CgiException(MESSAGE, new IllegalArgumentException());
     }
 
-    @org.junit.Test
+    @Test
     public void testExpectedException4() {
         cgiException.expect(CgiException.class);
         throw new CgiException(new IllegalArgumentException());
