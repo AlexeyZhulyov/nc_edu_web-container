ac96962add251dfc84f4ca9c91fa406660904e6e
vinogradov.max <vinogradov.max97@gmail.com>
2016-03-04 12:34:06 +0200

    Fixed dispatcher pom.xml. ServerDispatcher implemented Dispatcher. Added ServerDispatcherTest. #20

--- modules/dispatcher/src/main/java/nc/sumy/edu/webcontainer/dispatcher/ServerDispatcher.java
+++ modules/dispatcher/src/main/java/nc/sumy/edu/webcontainer/dispatcher/ServerDispatcher.java
@@ -1,8 +1,15 @@
 package nc.sumy.edu.webcontainer.dispatcher;
 
+import nc.sumy.edu.webcontainer.http.HttpResponse;
+
 /**
  * Class that takes a request, analyzes it and gives the output response.
  * @author Vinogradov Maxim
  */
-public class ServerDispatcher {
+public class ServerDispatcher implements Dispatcher{
+
+    @Override
+    public HttpResponse getResponse() {
+        return null;
+    }
 }
