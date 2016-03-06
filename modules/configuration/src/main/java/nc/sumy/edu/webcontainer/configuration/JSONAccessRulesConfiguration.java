2fc971a7bdc811728c678d56e3c58df028ea997e
OlehLuk <vadlukcis@gmail.com>
2016-03-06 16:15:54 +0200

    #3 Alpha version of AccessRules reading for security module

---
+++ modules/configuration/src/main/java/nc/sumy/edu/webcontainer/configuration/JSONAccessRulesConfiguration.java
@@ -0,0 +1,28 @@
+package nc.sumy.edu.webcontainer.configuration;
+
+import com.google.gson.Gson;
+import nc.sumy.edu.webcontainer.sequrity.AccessRules;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class JSONAccessRulesConfiguration implements AccessRulesConfiguration {
+
+
+    @Override
+    public AccessRules getAccessRules(String filePath) {
+        return getAccessRules(new File(filePath));
+    }
+
+    @Override
+    public AccessRules getAccessRules(File accessRulesFile) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(accessRulesFile));
+            return new Gson().fromJson(br, AccessRules.class);
+        } catch (FileNotFoundException e) {
+            return null;
+        }
+    }
+}
