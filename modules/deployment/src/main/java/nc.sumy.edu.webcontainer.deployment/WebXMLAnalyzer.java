97f1b7b7716079d7e1ca2968a92326a473ef2e29
vinogradov.max <vinogradov.max97@gmail.com>
2016-03-15 00:14:28 +0200

    [#31] Fixed mistakes in logic of class, now it's work right and parse file correctly.

--- modules/deployment/src/main/java/nc.sumy.edu.webcontainer.deployment/WebXMLAnalyzer.java
+++ modules/deployment/src/main/java/nc.sumy.edu.webcontainer.deployment/WebXMLAnalyzer.java
@@ -3,6 +3,7 @@ package nc.sumy.edu.webcontainer.deployment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.ErrorHandler;
 import org.xml.sax.SAXException;
@@ -18,29 +19,38 @@ import java.net.URI;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 import static java.io.File.separator;
 
 class WebXMLAnalyzer {
-    private static final Logger LOGGER = LoggerFactory.getLogger(WebXMLAnalyzer.class);
-    private static final String URL_PATTERN = "url-pattern";
-    private static final String SERVLET_PATH = "servlet-class";
-    private static final String WEB_XML = "web.xml";
-    private static final String CLASS_FOLDER = "classes";
+    private boolean isValid;
     private final File webInf;
     private final File webXml;
-    private boolean isValid;
     private Document document;
-    private Map<String, String>  sa = new HashMap<>();
-    private Map<String, String>  sa2 = new HashMap<>();
-    private Map<String, Class> warData = new HashMap<>();
+    private final File classPath;
+    private Map<String, String> servletTagMap = new HashMap<>();
+    private Map<String, String> servletMappingTagMap = new HashMap<>();
+    private Map<String, Class> dataMap = new HashMap<>();
+    /*Constants*/
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebXMLAnalyzer.class);
+    private static final String WEB_XML = "web.xml";
+    private static final String CLASS = "class";
+    private static final String SERVLET_TAG = "servlet";
+    private static final String SERVLET_MAPPING_TAG = "servlet-mapping";
+    private static final String SERVLET_NAME_TAG = "servlet-name";
+    private static final String SERVLET_CLASS_TAG = "servlet-class";
+    private static final String URL_PATTERN_TAG = "url-pattern";
 
-    public WebXMLAnalyzer(String webInf) {
-        this.webInf = new File(webInf);
-        webXml = new File(webInf + separator + WEB_XML);
+    public WebXMLAnalyzer(File webInf) {
+        this.webInf = webInf;
+        webXml = new File(webInf.toString() + separator + WEB_XML);
+        classPath = new File(webInf.toString() + separator + CLASS + separator);
         validateXMLbyDTD();
-        setData();
+        if (isValid()) {
+            makeDataMap();
+        }
     }
 
     private void validateXMLbyDTD() {
@@ -64,35 +74,53 @@ class WebXMLAnalyzer {
         isValid = true;
     }
 
-    private void setData() {
-        NodeList nodeList = document.getElementsByTagName(URL_PATTERN);
+    private void makeDataMap() {
+        getServletTagMap(SERVLET_TAG, SERVLET_CLASS_TAG, servletTagMap);
+        getServletTagMap(SERVLET_MAPPING_TAG, URL_PATTERN_TAG, servletMappingTagMap);
+        Class servletClass;
+        for (Map.Entry<String, String> item : servletTagMap.entrySet()) {
+            servletClass = createClass(servletMappingTagMap.get(item.getKey()));
+            dataMap.put(item.getValue(), servletClass);
+        }
     }
 
-    private Class createClass(File servletFile) {
-        NodeList nodeList = document.getElementsByTagName(URL_PATTERN);
-        String urlPattern = nodeList.item(0).getTextContent();
-        nodeList = document.getElementsByTagName(SERVLET_PATH);
-        String servletFolder = nodeList.item(0).getTextContent();
-        servletFile = new File(webInf + separator + CLASS_FOLDER + separator);
-        Class servletClass;
-        URI uri = null;
+    private void getServletTagMap(String innerTag, String childTag, Map<String, String> container) {
+        NodeList nodeList = document.getElementsByTagName(innerTag);
+        Element servletTag;
+        String servletName;
+        String infoTag;
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            servletTag = (Element) nodeList.item(i).getChildNodes();
+            servletName = servletTag.getElementsByTagName(SERVLET_NAME_TAG).item(0).getTextContent();
+            infoTag = servletTag.getElementsByTagName(childTag).item(0).getTextContent();
+            container.put(servletName, infoTag);
+        }
+    }
+
+    private Class createClass(String fullClassName) {
+        Class servletClass = null;
+        URI uri = classPath.toURI();
+        URL url;
         try {
-            uri = servletFile.toURI();
-            URL url = uri.toURL();
+            url = uri.toURL();
             ClassLoader classLoader = new URLClassLoader(new URL[]{url});
-            servletClass = classLoader.loadClass(servletFolder);
+            servletClass = classLoader.loadClass(fullClassName);
         } catch (MalformedURLException e) {
             LOGGER.warn("Malformed URL had occurred: ", e);
         } catch (ClassNotFoundException e) {
             LOGGER.warn("Class cannot be found: ", e);
         }
-        return null;
+        return servletClass;
     }
+
 
     public boolean isValid() {
         return isValid;
     }
 
+    public Map<String, Class> getDataMap() {
+        return dataMap;
+    }
     private class ParsingErrorHandler implements ErrorHandler {
         @Override
         public void warning(SAXParseException exception) {
@@ -112,5 +140,4 @@ class WebXMLAnalyzer {
             isValid = false;
         }
     }
-
 }
