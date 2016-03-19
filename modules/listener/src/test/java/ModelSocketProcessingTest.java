d1747304dfa8615b49c66ea1204a555636c9ef90
Oleh <vadlukcis@gmail.com>
2016-03-19 19:48:50 +0200

    Pmd fix

---
+++ modules/listener/src/test/java/ModelSocketProcessingTest.java
@@ -0,0 +1,16 @@
+import nc.sumy.edu.webcontainer.configuration.ServerConfigurationJson;
+import nc.sumy.edu.webcontainer.deployment.AutoDeployment;
+import nc.sumy.edu.webcontainer.dispatcher.ServerDispatcher;
+import nc.sumy.edu.webcontainer.listener.ModelSocketProcessing;
+import org.junit.Test;
+
+import java.net.Socket;
+
+public class ModelSocketProcessingTest {
+    ModelSocketProcessing model = new ModelSocketProcessing(new ServerDispatcher(new ServerConfigurationJson(),
+            new AutoDeployment()));
+    @Test
+    public void simpleSocketTest() {
+
+    }
+}
