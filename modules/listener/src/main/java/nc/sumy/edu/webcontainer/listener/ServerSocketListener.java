1ff24a36d15246e9e9790f94854c5fbfd53579e2
OlehLuk <vadlukcis@gmail.com>
2016-03-28 00:01:51 +0300

    [#36] Pmd fixes

--- modules/listener/src/main/java/nc/sumy/edu/webcontainer/listener/ServerSocketListener.java
+++ modules/listener/src/main/java/nc/sumy/edu/webcontainer/listener/ServerSocketListener.java
@@ -2,7 +2,6 @@ package nc.sumy.edu.webcontainer.listener;
 
 import nc.sumy.edu.webcontainer.configuration.ServerConfiguration;
 import nc.sumy.edu.webcontainer.deployment.Deployment;
-import nc.sumy.edu.webcontainer.dispatcher.ServerDispatcher;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,9 +10,9 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ServerSocketListener extends Thread {
-    private ServerSocket serverSocket;
+    private final ServerSocket serverSocket;
     private static final Logger LOGGER = LoggerFactory.getLogger(ServerSocketListener.class);
-    private ModelSocketProcessing model;
+    private final ModelSocketProcessing model;
     private boolean flag = true;
 
     public ServerSocketListener(ServerConfiguration configuration, Deployment deployment) throws IOException {
