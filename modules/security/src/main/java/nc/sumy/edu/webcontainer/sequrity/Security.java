0761b94948af57af4d4e23c867bf2cca954ad177
vinogradov.max <vinogradov.max97@gmail.com>
2016-03-28 00:45:00 +0300

    Added JavaDoc to the security module.

--- modules/security/src/main/java/nc/sumy/edu/webcontainer/sequrity/Security.java
+++ modules/security/src/main/java/nc/sumy/edu/webcontainer/sequrity/Security.java
@@ -1,7 +1,13 @@
 package nc.sumy.edu.webcontainer.sequrity;
 
+/**
+ * Interface that provides method for working with http-access.json files.
+ */
 public interface Security {
 
+    /**
+     * @return is http-access.json allow to show this page.
+     */
     boolean isAllow();
 
 }
