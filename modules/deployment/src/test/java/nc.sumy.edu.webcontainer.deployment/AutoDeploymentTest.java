253e98048201924495a1bf4d5ef02ca9af65636b
AlexeyZhulyov <alexey.zhulyov@gmail.com>
2016-03-28 14:41:49 +0300

    [#31] Fix pmd for deployment module tests

--- modules/deployment/src/test/java/nc.sumy.edu.webcontainer.deployment/AutoDeploymentTest.java
+++ modules/deployment/src/test/java/nc.sumy.edu.webcontainer.deployment/AutoDeploymentTest.java
@@ -1,15 +1,5 @@
 package nc.sumy.edu.webcontainer.deployment;
 
 public class AutoDeploymentTest {
-/*
-    @Test
-    public void deploymentTest() {
-
-       ServerConfiguration configuration = new ServerConfigurationJson();
-        configuration.setWwwLocation("src" + File.separator +
-                "test" + File.separator + "resources" + File.separator + "folder_listener");
-        AutoDeployment autoDeployment = new AutoDeployment(configuration);
-        autoDeployment.run();
-    }*/
 
 }
