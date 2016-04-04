6cd0fdf2eace43559668a03a071965e2592a0150
OlehLuk <vadlukcis@gmail.com>
2016-04-04 21:51:14 +0300

    Tests for listener improved

--- modules/listener/src/test/java/nc/sumy/edu/webcontainer/listener/ServerSocketListenerTest.java
+++ modules/listener/src/test/java/nc/sumy/edu/webcontainer/listener/ServerSocketListenerTest.java
@@ -1,4 +1,33 @@
 package nc.sumy.edu.webcontainer.listener;
 
+
+import nc.sumy.edu.webcontainer.configuration.ServerConfiguration;
+import nc.sumy.edu.webcontainer.configuration.ServerConfigurationJson;
+import nc.sumy.edu.webcontainer.deployment.Deployment;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
 public class ServerSocketListenerTest {
+    //create
+    ServerConfiguration config = new ServerConfigurationJson();
+    Deployment deployment = mock(Deployment.class);
+
+    @Test
+    public void serverSocketListnerCreationTest() throws IOException {
+        new ServerSocketListener(config, deployment);
+    }
+
+    //create on used port
+    @Test(expected = IOException.class)
+    public void listenerCreationOnUsedPortTest() throws IOException {
+        new ServerSocket(7121);
+        config.setPort(7121);
+        new ServerSocketListener(config, deployment);
+    }
+
 }
