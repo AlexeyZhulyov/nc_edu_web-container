c71e83b1d6cbf6f69220b3a662aef787f6ac19d7
vinogradov.max <vinogradov.max97@gmail.com>
2016-04-05 01:33:26 +0300

    Final tests to deployment added.

--- modules/deployment/src/test/java/nc.sumy.edu.webcontainer.deployment/AutoDeploymentTest.java
+++ modules/deployment/src/test/java/nc.sumy.edu.webcontainer.deployment/AutoDeploymentTest.java
@@ -15,9 +15,10 @@ import static org.junit.Assert.assertEquals;
 
 public class AutoDeploymentTest {
     private static final String WWW_PATH = "src/test/resources/folder_listener";
-    private static final String DEMO_PATH = WWW_PATH + separator + "www" + separator + "Servlets_demo-master";
+    private static final String DEMO_PATH = WWW_PATH + separator + "www";
     private AutoDeployment deployment;
     private final static String BLOG = "/Blog";
+    private final static String DELETED_BLOG = "/Deleted";
 
     @Before
     public void setInitData() {
@@ -30,13 +31,18 @@ public class AutoDeploymentTest {
     @After
     public void finishDeploy() {
         deployment.interrupt();
-        deleteQuietly(new File(DEMO_PATH));
+        deleteQuietly(new File(DEMO_PATH + separator + "Servlets_demo-master"));
+        deleteQuietly(new File(DEMO_PATH + separator + "sample"));
     }
 
     @Test
     public void testDeploymentOnStart() {
         ConcurrentHashMap<File, ConcurrentHashMap<String, Class>> domainsData = deployment.getDomainsData();
-        ConcurrentHashMap<String, Class> dataMap = domainsData.get(new File(DEMO_PATH));
+        ConcurrentHashMap<String, Class> dataMap = domainsData.get(new File(DEMO_PATH + separator + "Servlets_demo-master"));
         assertEquals(dataMap.containsKey(BLOG), true);
+        assertEquals(dataMap.containsKey(DELETED_BLOG), false);
+        dataMap = domainsData.get(new File(DEMO_PATH + separator + "sample"));
+        assertEquals(dataMap.containsKey(BLOG), false);
+        assertEquals(dataMap.containsKey(DELETED_BLOG), true);
     }
 }
