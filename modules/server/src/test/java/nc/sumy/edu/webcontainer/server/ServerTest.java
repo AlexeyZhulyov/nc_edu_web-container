4fea93e317abbbc28816d48cade3660a180c18a6
OlehLuk <vadlukcis@gmail.com>
2016-04-05 14:31:21 +0300

    Server tests added

--- modules/server/src/test/java/nc/sumy/edu/webcontainer/server/ServerTest.java
+++ modules/server/src/test/java/nc/sumy/edu/webcontainer/server/ServerTest.java
@@ -2,13 +2,41 @@ package nc.sumy.edu.webcontainer.server;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class ServerTest {
     @Test
-    public void maiTest() throws InterruptedException {
+    public void mainSimulateTest() throws InterruptedException {
         System.setProperty("SERVER_HOME", "src\\test\\resources");
         Server server = new Server();
         server.startServer();
         Thread.sleep(3000);
         server.stopServer();
+    }
+
+    @Test(expected = ServerFailException.class)
+    public void startServerWithoutSystemVariableTest() {
+        new Server();
+    }
+
+    @Test(expected = ServerFailException.class)
+    public void startServerWithBadSystemVariableTest()  {
+        System.setProperty("SERVER_HOME", "src\\test\\resources\\badpath");
+        new Server();
+    }
+
+    @Test(expected = ServerFailException.class)
+    public void systemVariableAtFileTest() {
+        System.setProperty("SERVER_HOME", "src\\test\\resources\\test.txt");
+        new Server();
+    }
+
+    @Test(expected = ServerFailException.class)
+    public void usedPortTest() throws  IOException {
+        try(ServerSocket serverSocket = new ServerSocket(8096)){
+            System.setProperty("SERVER_HOME", "src\\test\\resources");
+            new Server();
+        }
     }
 }
